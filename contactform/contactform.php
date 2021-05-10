c669ed3d01c89d2b041161826e66cd680a0298f1
Ricky <ricky@studiohakuna.com>
2021-05-10 02:02:58 -0300

    Contact Form

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -8,7 +8,7 @@
 /***************** Configuration *****************/
 
   // Replace with your real receiving email address
-  $contact_email_to = "induspacsa@gmail.com";
+  $contact_email_to = "rickyvadala@gmail.com";
 
   // Title prefixes
   $subject_title = "Asunto:";
